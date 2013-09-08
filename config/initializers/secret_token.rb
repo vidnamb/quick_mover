01441b533e288d2919d192728b54c2123006229b
Vidya Nambiar <vnambiar@andrew.cmu.edu>
2013-09-08 14:19:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickMover::Application.config.secret_key_base = 'f2ce0750206f6f81623dce62df58e967ec132d515dd90a9ad04aa72a98c12148ecd74723297e18cf8bbc5169c253c91f1b34a2d16ad63b1218cf27c258ccf32a'
